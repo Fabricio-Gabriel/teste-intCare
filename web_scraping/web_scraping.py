d6d8ad096e685b054ac2aadafb645c3a29acbee8
Fabricio Gabriel <fabricio.melquiades04@gmail.com>
2025-04-03 15:02:48 -0300

    Executando download de uma página em PDF com selenium e wget

--- web_scraping/web_scraping.py
+++ web_scraping/web_scraping.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import wget
+import os
 
 URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 navegador = webdriver.Chrome()
@@ -10,6 +12,7 @@ navegador.get(URL)
 btn_cookies = navegador.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div/div[2]/button[3]')
 btn_cookies.click()
 
+
 # baixa o primeiro PDF
 primeiro_pdf = navegador.find_element(By.XPATH, '//*[@id="cfec435d-6921-461f-b85a-b425bc3cb4a5"]/div/ol/li[1]/a[1]')
 primeiro_pdf.click()
@@ -17,19 +20,13 @@ primeiro_pdf.click()
 navegador.switch_to.window(navegador.window_handles[-1])
 
 url_pagina1 = navegador.current_url
-print(f'estou na url: {url_pagina1}')
 
-download_primeiro_pdf = navegador.find_element(By.XPATH, '//*[@id="icon"]')
-script_primeiro_pdf = navegador.execute_script('return arguments[0].shadowRoot', download_primeiro_pdf)
-download = script_primeiro_pdf.find_element(By.CSS_SELECTOR, 'cr-icon')
-download.click()
+pasta_destino = "C:Users/Fabri/Downloads/"
 
-# baixa o segundo PDF
-'''segundo_pdf = navegador.find_element(By.XPATH, '//*[@id="cfec435d-6921-461f-b85a-b425bc3cb4a5"]/div/ol/li[2]/a')
-segundo_pdf.click()'''
+download_1 = wget.download(url_pagina1, "anexo_1_rol.pdf")
 
-'''download_segundo_pdf = navegador.find_element(By.XPATH, '//*[@id="icon"]/cr-icon')
-download_segundo_pdf.click()'''
+# baixa o segundo pdf
+
 
 input("Pressione Enter para fechar... ")
 navegador.quit()
